e1e42faeda1821d29c390f1cdbf098a33a490143
GajenderTyagi <gajender@programmers.io>
2020-06-19 09:50:40 +0530

    mongo db added

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -4,3 +4,21 @@ const Employee = require('../models/employee.model');
 exports.test = function (req, res) {
   res.send('Greetings from the Test controller!');
 };
+
+exports.employee_create = function (req, res) {
+  let employee = new Employee(
+    {
+      id: req.body.id,
+      name: req.body.name,
+      team: req.body.team,
+      age: req.body.age
+    }
+  );
+
+  employee.save(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.send('Employee Created successfully');
+  });
+};
